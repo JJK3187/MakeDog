9b8b6e4dee187374cbf0189e6ea0bfd3c43ac0dc
JJK3187 <truestory14@kakao.com>
2025-12-12 17:10:49 +0900

    캡슐화 진행 및 step 메서드 작성

--- src/makeDog/Dog.java
+++ src/makeDog/Dog.java
@@ -2,11 +2,21 @@ package makeDog;
 
 public class Dog {
 
-    String name;
-    String breed;
+    private String name;
+    private String breed;
 
     public Dog(String name, String breed) {
         this.name = name;
         this.breed = breed;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getBreed() {
+        return breed;
+    }
+    public String step() {
+        System.out.println(name + "가 걸어갑니다.");
+        return name;
     }
 }
